606b5ccf14825cc486cff4c5be0de532f2cced0d
emrahhcan <emrahcansahinoglu@gmail.com>
2022-03-23 09:46:08 +0300

    Fixed html

--- assets/js/simpleScripts.js
+++ assets/js/simpleScripts.js
@@ -2,6 +2,7 @@
 
 /* *** CALL DECLARED FUNCTIONS *** */
 togglerMenu();
+correctAnswer();
 
 /* *** FUNCTIONS *** */
 function togglerMenu() {
@@ -10,5 +11,31 @@ function togglerMenu() {
 
     hamburgerMenuBtn.addEventListener('click', function() {
         navigation.classList.toggle('show-links');
+    });
+}
+
+function correctAnswer() {
+    const book = document.querySelectorAll(".book");
+
+    book.forEach(element => {
+        element.addEventListener('click', () => {
+            if(
+                (book[1] === element) || 
+                (book[2] === element) || 
+                (book[4] === element) || 
+                (book[7] === element) || 
+                (book[8] === element) || 
+                (book[11] === element) || 
+                (book[13] === element) || 
+                (book[14] === element) || 
+                (book[17] === element) || 
+                (book[18] === element)
+            ) {
+                element.classList.add("correct");
+            } 
+            else {
+                element.classList.add("wrong");
+            }
+        });
     });
 }
